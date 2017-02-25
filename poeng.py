468db29c71e212bf066f288bc35c1d5d0485fca6
Jon Terner <jon.programming@gmail.com>
2017-02-25 15:43:43 -0500

    stuff

---
+++ poeng.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Main source file for poeng game
+'''
+
+import pygame
+from pygame.locals import *
+
+class Game:
+    def on_execute(self):
+        print("poop")
+        pass
+
+if __name__ == "__main__" :
+    game = Game()
+    game.on_execute()
