39fa433e21efd4976d526f3bd3390e33f84d4d9d
BradleyM <bradleym27@gmail.com>
2017-02-25 21:18:17 -0500

    paddle beginning

--- paddle.py
+++ paddle.py
@@ -1,2 +1,22 @@
 import pygame
+from gameobj import GameObject
 from pygame.locals import *
+
+#This is a paddle object
+class Paddle(GameObject):
+    def __init__(self, x, y, size)
+        GameObject.__init__(self)
+        self.image = pygame.image.load('assets/paddle.gif').convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = x + 1
+        self.rect.y = y
+        self.speed = 3; #need to change this according to how fast we want paddle to move
+        self.screen_width =  size[0]
+        self.screen_height = size[1]
+        self.direction = 
+
+    def update(self):
+        pass
+
+    def move(self):
+        pass
