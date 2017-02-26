c02e298d602370e64d0989fec68f55f231396b12
Jon Terner <jon.programming@gmail.com>
2017-02-25 23:08:21 -0500

    spicy

--- infobar.py
+++ infobar.py
@@ -10,11 +10,10 @@ class HUD(object):
         self.screen = screen
         self.bgColor = (150, 150, 150)
         self.height = 70 # height above bottom (y = 960)
-        self.text = Text(0,960, str(0))
+        self.text = Text(0,0, str(0))
 
     def draw(self):
-        self.text.draw(self.screen)
-        pass
+        self.screen.blit(self.text.image, self.text.rect)
 
 
 
